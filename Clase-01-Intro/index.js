c1241b0e020b20480ca14e35bb2391b6e7dda65d
Marce Calle <marcelo.callejo@gmail.com>
2023-06-05 12:13:02 -0300

    clase 1 y 2

---
+++ Clase-01-Intro/index.js
@@ -0,0 +1,5 @@
+const FECHA = 2023
+let fechaNacimiento = Number( prompt('Ingresa año de nacimiento') )
+let edad = FECHA - fechaNacimiento
+console.log('Tengo ' + edad + ' años.')
+alert('Tengo ' + edad + ' años.') 
