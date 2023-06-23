a0a08c1241e0510e178dd16511d1c1379694503e
marcecalle <38355063+marcecalle@users.noreply.github.com>
2023-06-23 18:29:12 -0300

    actividade ejercicio 2

--- actividades-complementarias/actividades.js
+++ actividades-complementarias/actividades.js
@@ -1,3 +1,5 @@
+//! EJERCICIO 1
+
 //* Actividad 1 (HOMERO)
 
 // let firstName
@@ -18,25 +20,118 @@
 
 //* Actividad 3 (CARNET)
 
-let idNumber   = 'B47U89RE243'
-let name       = 'BART SIMPSON'
-let address    = '742 EVERGREEN TERR'
-let city       = 'SPRINGFIELD'
-let birthDate  = '02-11-70'
-let sex        = 'M'
-let height     = '4.0'
-let eyes       = 'BL'
-let weight     = '85'
+// let idNumber   = 'B47U89RE243'
+// let fullName       = 'BART SIMPSON'
+// let address    = '742 EVERGREEN TERR'
+// let city       = 'SPRINGFIELD'
+// let birthDate  = '02-11-70'
+// let sex        = 'M'
+// let height     = '4.0'
+// let eyes       = 'BL'
+// let weight     = '85'
 
-let carnet     = 
-      'Numero de ID: ' + idNumber + ' '
-   +  'Nombre: ' + name + ' '
-   +  'Domicilio: ' + address + ' '
-   +  'Ciudad: ' + city + ' '
-   +  'Fecha de Nacimiento: ' + birthDate + ' '
-   +  'Sexo: ' + sex + ' '
-   +  'Altura: ' + height + ' '
-   +  'Color de ojos: ' + eyes + ' '
-   +  'Peso: ' +  weight
+// let carnet     = 
+//       'Numero de ID: ' + idNumber + ' '
+//    +  'Nombre: ' + fullName + ' '
+//    +  'Domicilio: ' + address + ' '
+//    +  'Ciudad: ' + city + ' '
+//    +  'Fecha de Nacimiento: ' + birthDate + ' '
+//    +  'Sexo: ' + sex + ' '
+//    +  'Altura: ' + height + ' '
+//    +  'Color de ojos: ' + eyes + ' '
+//    +  'Peso: ' +  weight
 
-console.log(carnet)
+// console.log(carnet)
+
+//* Actividad 4 (REGISTRO DE PERSONAS)
+
+// let member1 = prompt('Ingrese el nombre de un/una integrante de la familia');
+// let member2 = prompt('Ingrese el nombre de un/una integrante de la familia');
+// let member3 = prompt('Ingrese el nombre de un/una integrante de la familia');
+// let member4 = prompt('Ingrese el nombre de un/una integrante de la familia');
+// let member5 = prompt('Ingrese el nombre de un/una integrante de la familia');
+
+// alert('Integrantes de la familia: '
+//       + member1 + ', '
+//       + member2 + ', '
+//       + member3 + ', '
+//       + member4 + ', '
+//       + member5 + '.')
+
+//* Actividad 5 (DESCUENTOS)
+
+// let priceA = parseFloat(prompt('Ingrese el precio al que aplicar el descuento.'))
+// let discount = Number(prompt('Ingrese el monto del descuento: '))
+// let finalPrice = priceA - ((discount * priceA) / 100)
+// console.log('El precio final con descuento es: $' + finalPrice)
+
+//! EJERCICIO 2
+
+//* Actividad 1 (YO NO FUI)
+
+// let name1 = 'Nelson'
+// let name2 = prompt('Ingrese un nombre')
+
+// if (name2 === name1) {
+//    alert('Fuí yo')
+// } else {
+//    alert('Yo no fuí')
+// }
+
+//* Actividad 2 (PRESIONAR Y)
+
+// let letter = prompt('Ingrese una letra:')
+// if (letter === 'y' || letter === 'Y') {
+//    alert('Correcto')
+// } else {
+//    alert('Error')
+// }
+
+//* Actividad 3 (ESCOGER PERSONAJE)
+
+// let option = Number(prompt('Elige una opción entre 1, 2, 3 y 4.'))
+// if (option === 1) {
+//    console.log('Elegiste a Homero');
+// } 
+// if (option === 2) {
+//    console.log('Elegiste a Marge');
+// }
+// if (option === 3) {
+//    console.log('Elegiste a Bart');
+// }
+// if (option === 4) {
+//    console.log('Elegiste a Lisa');
+// } else {
+//    alert('Error!')
+// }
+
+//* Actividad 4 (PRESUPUESTO)
+
+// let number = Number(prompt('Ingrese un número'))
+// if ((number >= 0) && (number <= 1000)) {
+//    console.log('Presupuesto bajo.')
+// } else if ((number > 1000) && (number <= 3000)) {
+//    console.log('Presupuesto medio.')
+// } else if (number > 3000) {
+//    console.log('Presupuesto alto.');
+// } else {
+//    console.log('Error!');
+// }
+
+//* Actividad 5 (VACÍO)
+
+let product1 = prompt('Ingrese producto de alamacén 1:')
+let product2 = prompt('Ingrese producto de alamacén 2:')
+let product3 = prompt('Ingrese producto de alamacén 3:')
+let product4 = prompt('Ingrese producto de alamacén 4:')
+
+if ((product1 !== '') && (product2 !== '') && (product3 !== '') && (product4 !== '')) {
+   alert(
+      'Los productos son: '
+      + product1 + ', '
+      + product2 + ', '
+      + product3 + ', '
+      + product4 + '.')
+} else {
+   alert('Error: Es necesario cargar todos los productos')
+}
